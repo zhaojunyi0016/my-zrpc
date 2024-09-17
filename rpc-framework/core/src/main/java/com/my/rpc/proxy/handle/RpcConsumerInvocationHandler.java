aedb4fed74e6dec056093e263fbea2a3c49b67be
赵俊逸 <451108902@qq.com>
2024-09-17 22:54:11 +0800

    修改

--- rpc-framework/core/src/main/java/com/my/rpc/proxy/handle/RpcConsumerInvocationHandler.java
+++ rpc-framework/core/src/main/java/com/my/rpc/proxy/handle/RpcConsumerInvocationHandler.java
@@ -215,10 +215,7 @@ public class RpcConsumerInvocationHandler implements InvocationHandler {
 
     public static void main(String[] args) {
 
-        long batchSize = (20159648 / 10000) + 1;
-        for (int i = 0; i <= batchSize; i++) {
-            System.out.println(i * 1000);
-        }
+
     }
 
 }
